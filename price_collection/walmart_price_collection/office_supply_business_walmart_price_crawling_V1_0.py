20520e15b401aee60316d6ec1f4c27a532a93a59
Sathiesh Suyambudhasan <sathiesh.dhas@gmail.com>
2023-02-12 18:19:11 +0530

    new script for business walmart scrape

---
+++ price_collection/walmart_price_collection/office_supply_business_walmart_price_crawling_V1_0.py
@@ -0,0 +1,356 @@
+import logging
+import os
+import requests
+import time
+import urllib3
+import json
+import random
+import urllib.request
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+IDENTIFICATION_FILE = "identification.txt"
+INPUT_FILE = "input.txt"
+OUTPUT_FILE = "output.txt"
+ERROR_FILE = "error.txt"
+NOTFOUND_FILE = "notfound.txt"
+
+mm_dd_yyyy = datetime.now().strftime('%m-%d-%Y')
+
+# proxies = {
+#     'http': 'http://user-83896:Strike@123@205.164.3.104:1212'
+#     # 'http': 'http://dhaskarthick:Q<R[8N0@4w)$@gw.ntnt.io:5959'
+# }
+
+user_agents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36']
+
+urllib3.disable_warnings()
+
+
+def log_and_console_info(message: str):
+    print(f'INFO : {message}')
+    logging.info(f'INFO : {message}')
+
+
+def log_and_console_error(message: str):
+    print(f'ERROR : {message}')
+    logging.error(f'ERROR : {message}')
+
+
+def prepare_reuest_client():
+    proxy = urllib.request.ProxyHandler({'https': 'http://user-83896:Strike$123@205.164.3.104:1212'})
+    opener = urllib.request.build_opener(proxy)
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
+
+
+def call_url(url: str):
+    while True:
+        try:
+            proxy_handler = urllib.request.ProxyHandler({'http': 'http://user-83896:Strike@123@205.164.3.104:1212'})
+            opener = urllib.request.build_opener(proxy_handler)
+            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+            urllib.request.install_opener(opener)
+            content = urllib.request.urlopen(url).read().decode()
+
+            if content.__contains__('Robot or human?'):
+                logging.error('Blocked by walmart!')
+                raise Exception('Blocked')
+
+            return content
+
+        except Exception as exception:
+            logging.error(exception, exc_info=True)
+            wait_till(30)  # wait 2 mins before retry
+
+
+def http_client():
+    session = requests.Session()
+    session.proxies = {
+        'http': 'http://user-83896:Strike@123@205.164.3.104:1212',
+        'https': 'http://user-83896:Strike@123@205.164.3.104:1212'
+    }
+    # session.trust_env = False
+    session.headers.update(
+        {
+            # "User-Agent": random.choice(user_agents),
+            "User-Agent": 'PostmanRuntime/7.30.1',
+            "referer": "https://www.google.com"
+        }
+    )
+
+    def log_url(res, *args, **kwargs):
+        logging.info(f"{res.url}, {res.status_code}")
+
+    session.hooks["response"] = log_url
+    return session
+
+
+def make_request(session: requests.Session, url: str):
+    while True:
+        try:
+            response = session.get('https://www.tutorialspoint.com/python-program-to-find-the-ip-address-of-the-client', verify=False)
+
+            if response.status_code == 200:
+                return response
+            elif response.status_code == 429:
+                print_captcha_error()
+                log_and_console_error("Quiting - CAPTCHA Occurred!")
+                exit("Quiting - Captcha occurred!")
+            else:
+                return
+        except requests.exceptions.RequestException as req_exception:
+            logging.error(req_exception, exc_info=True)
+            wait_till(10)  # wait 2 mins before retry
+
+
+def wait_till(duration: int):
+    log_and_console_info(f'Sleeping for {duration} seconds.')
+
+    for i in range(1, duration):
+        print(f'Resuming in {duration - i} seconds...')
+        time.sleep(1)
+
+
+def print_data_count():
+    input_count = output_count = error_count = notfound_count = 0
+    with open(INPUT_FILE, 'r', encoding='utf8', errors='replace') as input_file:
+        input_count = len(input_file.readlines())
+
+    if(os.path.exists(OUTPUT_FILE)):
+        with open(OUTPUT_FILE, 'r', encoding='utf8', errors='replace') as output_file:
+            output_count = len(output_file.readlines())
+            output_count = output_count - 1  # Excluding the titles
+
+    if(os.path.exists(ERROR_FILE)):
+        with open(ERROR_FILE, 'r', encoding='utf8', errors='replace') as error_file:
+            error_count = len(error_file.readlines())
+
+    if(os.path.exists(NOTFOUND_FILE)):
+        with open(NOTFOUND_FILE, 'r', encoding='utf8', errors='replace') as notfound_file:
+            notfound_count = len(notfound_file.readlines())
+
+    log_and_console_info("\n\n")
+    log_and_console_info("########################################")
+    log_and_console_info(f"##### Total Input is     : {input_count}")
+    log_and_console_info(f"##### Total Output is    : {output_count}")
+    log_and_console_info(f"##### Total Error is     : {error_count}")
+    log_and_console_info(f"##### Total Not Found is : {notfound_count}")
+    log_and_console_info("########################################")
+
+
+def print_captcha_error():
+
+    log_and_console_error("CAPTCHA Occurred!")
+
+    log_and_console_error('   _____              _____    _______    _____   _    _             ')
+    log_and_console_error('  / ____|     /\     |  __ \  |__   __|  / ____| | |  | |     /\     ')
+    log_and_console_error(' | |         /  \    | |__) |    | |    | |      | |__| |    /  \    ')
+    log_and_console_error(' | |        / /\ \   |  ___/     | |    | |      |  __  |   / /\ \   ')
+    log_and_console_error(' | |____   / ____ \  | |         | |    | |____  | |  | |  / ____ \  ')
+    log_and_console_error('  \_____| /_/    \_\ |_|         |_|     \_____| |_|  |_| /_/    \_\ ')
+
+
+def write_into_error_file(input_record_splits: list):
+    error_file = open(ERROR_FILE, "a")
+
+    flag = False
+    for input_split in input_record_splits:
+        if flag:
+            error_file.write("\t")
+        flag = True
+        error_file.write(str(input_split).strip())
+
+    error_file.write("\n")
+    error_file.close()
+
+
+def write_into_notfound_file(input_record_splits: list):
+    notfound_file = open(NOTFOUND_FILE, "a")
+
+    flag = False
+    for input_split in input_record_splits:
+        if flag:
+            notfound_file.write("\t")
+        flag = True
+        notfound_file.write(str(input_split).strip())
+
+    notfound_file.write("\n")
+    notfound_file.close()
+
+
+def update_identification_file(strike_id: str):
+    identification_file = open(IDENTIFICATION_FILE, "a")
+    identification_file.write(strike_id + "\t" + str(datetime.now()) + "\n")
+    identification_file.close()
+
+
+def get_identification_value():
+    log_and_console_info(f"Opening identification file {IDENTIFICATION_FILE}")
+    indent_ids = []
+    idents = []
+    last_id = 'none'
+    if(os.path.exists(IDENTIFICATION_FILE)):
+        with open(IDENTIFICATION_FILE, 'r') as f:
+            idents = f.readlines()
+        if(len(idents)):
+            for ident in idents:
+                indent_ids.append(ident.split('\t')[0])
+
+            last_id = indent_ids[-1]
+    return last_id
+
+
+def create_output_file():
+    if(not os.path.exists(OUTPUT_FILE)):
+        output_file = open(OUTPUT_FILE, "a")
+        output_file.write("Strike ID\tUnique ID\tInventory Number/SKU\tTitle\tPrice\tReview\tRating\tImage")  # Merchant 1	Merchant Price 1	Shipping 1
+        output_file.write("\n")
+        output_file.close()
+
+
+def open_inputs_from_file(filename: str):
+    log_and_console_info(f"Opening input file {filename}")
+    lines = []
+    with open(filename, 'r') as f:
+        inputs = f.readlines()
+    for input_record in inputs:
+        if(input_record.strip() != ''):
+            lines.append(input_record.strip().split('\t'))
+    return lines
+
+
+def extract_next_urls(response: requests.Response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    next_urls = []
+    try:
+        for next_url_element in soup.select('#sh-fp__pagination-button-wrapper > span > a'):
+            next_urls.append(next_url_element.attrs['data-url'])
+
+    except AttributeError as attribute_error:
+        logging.error(attribute_error, exc_info=True)
+    return next_urls
+
+
+# def extract_data(response: requests.Response):
+def extract_data(content: str):
+
+    # log_and_console_info(response.text)
+    title = sku = gtin13 = brand = model = price = availability = item_condition = delivery = reviews = rating = image = 'na'
+    # soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(content, 'lxml')
+
+    # Saving the html content as a html file
+    # with open(strike_id+'.html', 'w', encoding='utf-8') as f:
+    #     f.write(str(soup))
+
+    try:
+        data = (json.loads(soup.find('script', attrs={'type': 'application/ld+json'}).text))
+
+        title = data['name']
+        sku = data['sku']
+        gtin13 = data['gtin13']
+        brand = data['brand']['name']
+        model = data['model']
+        image = data['image']
+
+        if 'offers' in data:
+            offers = data['offers']
+            availability = str(offers['availability']).split('/')[-1]
+            item_condition = str(offers['itemCondition']).split('/')[-1]
+            delivery = str(offers['availableDeliveryMethod']).split('/')[-1]
+
+            if 'price' in offers:
+                price = data['offers']['price']
+        if 'aggregateRating' in data:
+            reviews = data['aggregateRating']['reviewCount']
+            rating = data['aggregateRating']['ratingValue']
+
+    except AttributeError as attribute_error:
+        logging.error(attribute_error, exc_info=True)
+        return None
+
+    log_and_console_info(
+        f'Seller [title={title}, sku={sku}, gtin13={gtin13}, brand={brand}, model={model}, price={price}, availability={availability}, item_condition={item_condition}, delivery={delivery}, reviews={reviews}, rating={rating}, image={image}]')
+    return (title, sku, gtin13, brand, model, price, availability, item_condition, delivery, reviews, rating, image)
+
+
+def main():
+    logging.basicConfig(filename='app.log', format='%(asctime)s %(message)s', level=logging.INFO)
+    log_and_console_info(f'Last seen date is {mm_dd_yyyy}')
+    program_start_time = datetime.now()
+    log_and_console_info(f"##### Starting the crawling at {program_start_time}")
+
+    # Create the output file with headers
+    create_output_file()
+
+    # client = http_client()
+    # prepare_reuest_client()
+
+    # Returns 'none' if there is no identification file created
+    last_id = get_identification_value()
+    log_and_console_info(f'last id : {last_id}')
+
+    inputs = open_inputs_from_file(INPUT_FILE)
+    total_input_count = len(inputs)
+
+    # Looping the list of inputs as range to get the index of the input
+    for i in range(len(inputs)):
+        input_record = inputs[i]
+        strike_id = input_record[0].strip()
+        ###################### Identification block ######################
+        # If last_id is 'none' [identification file not created] or 'found_last_value' [last value in identification is ound in input file], then start program
+        # If last_id is a valid input id and matched with input list, then set found_last_value to last_id
+        if(last_id != 'none' and last_id != 'found_last_value'):
+            if(last_id != strike_id):
+                continue  # Skipping the input if that is not the last value written in identification file
+            elif(last_id == strike_id):
+                last_id = 'found_last_value'
+                continue  # Setting new value and Skipping the input last value, if that is the last value written in identification file
+
+        log_and_console_info(f"################ Processing input {i+1} of {total_input_count} ################")
+
+        unique_id = input_record[1].strip()
+        inventory_no = input_record[2].strip()
+        url_in = input_record[3].strip()
+        url = url_in.split('?')[0]
+
+        log_and_console_info(f"Searching product with id = {strike_id} and url {url}")
+        time.sleep(0.5)  # Sleeping for 0.5 second
+        start = datetime.now()
+        # html_response = make_request(client, url_in)
+        # log_and_console_info(html_response.content)
+        # log_and_console_info(f'Searching product with url {url}')
+        log_and_console_info("Sleeping for 15 seconds!")
+        time.sleep(15)  # sleeping for 30 seconds
+        web_content = call_url(url)
+        # log_and_console_info(web_content)
+
+        # if html_response is None:
+        if web_content is None:
+            log_and_console_error("Error Occurred!")
+            write_into_error_file(input_record)
+            continue  # Skipping after writing into error file
+        else:
+            # (title, price, reviews, rating, image) = extract_data(html_response)
+            (title, sku, gtin13, brand, model, price, availability, item_condition, delivery, reviews, rating, image) = extract_data(web_content)
+
+            if(price != 'na'):
+                output_file = open(OUTPUT_FILE, "a", encoding='utf8', errors='ignore')
+                output_file.write(strike_id + "\t" + unique_id + "\t" + inventory_no + "\t" + title + "\t" + str(sku) + "\t" + str(gtin13) + "\t" + brand + "\t" + str(model) +
+                                  "\t" + str(price) + "\t" + availability + "\t" + item_condition + "\t" + delivery + "\t" + str(reviews) + "\t" + str(rating) + "\t" + image + "\n")
+                output_file.close()
+
+            log_and_console_info(f'Time taken {datetime.now() - start}')
+
+        update_identification_file(strike_id)
+
+    log_and_console_info(f"##### Program execution time is {datetime.now() - program_start_time} seconds")
+    log_and_console_info('--FINISHED--')
+
+    print_data_count()
+
+
+if __name__ == '__main__':
+    main()
